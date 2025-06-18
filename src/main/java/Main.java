340c59877fbe347aa7d672caee84ac87b0028c9a
kuduablik <job_0@bk.ru>
2025-06-19 03:50:54 +0400

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Как вас зовут? ");
+        String name = scanner.nextLine();
+        System.out.println("Рад познакомиться, " + name + "! 😊");
+    }
+}
